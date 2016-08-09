14ec6ad014e98d5d9fe3042f24c5c0448e37d742
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2016-08-09 14:21:24 +0200

    Port 'Message' widget to Python/Orange 3

--- _textable/widgets/OWTextableMessage.py
+++ _textable/widgets/OWTextableMessage.py
@@ -20,55 +20,44 @@ along with Orange-Textable v2.0. If not, see <http://www.gnu.org/licenses/>.
 
 __version__ = '0.01.2'
 
-"""
-<name>Message</name>
-<description>Parse JSON data in segmentation and use them to control other
-widgets</description>
-<icon>icons/Message.png</icon>
-<priority>10002</priority>
-"""
 
 import json
 from LTTL.Segmentation import Segmentation
-from TextableUtils import *
+from .TextableUtils import (
+    OWTextableBaseWidget, VersionedSettingsHandler,
+    JSONMessage, SendButton, InfoBox, pluralize
+)
 
-from Orange.OrangeWidgets.OWWidget import *
-import OWGUI
+from Orange.widgets import gui
 
 
-class OWTextableMessage(OWWidget):
+class OWTextableMessage(OWTextableBaseWidget):
     """Orange widget for parsing JSON data in segmentation"""
 
-    settingsList = [
-        'autoSend',
-        'uuid',
-    ]
+    name = "Message"
+    description = "Parse JSON data in segmentation and use them to control" \
+                  "other widgets"
+    icon = "icons/Message.png"
+    priority = 10002
 
-    def __init__(self, parent=None, signalManager=None):
+    inputs = [("Segmentation", Segmentation, "inputData")]
+    outputs = [("Message", JSONMessage)]
 
+    settingsHandler = VersionedSettingsHandler(
+        version=__version__.split(".")[:2]
+    )
+
+    want_main_area = False
+    # TODO: wantStateInfoWidget = 0
+
+    def __init__(self, *args, **kwargs):
         """Initialize a Message widget"""
-
-        OWWidget.__init__(
-            self,
-            parent,
-            signalManager,
-            wantMainArea=0,
-            wantStateInfoWidget=0,
-        )
-
-        self.inputs = [("Segmentation", Segmentation, self.inputData, Single)]
-        self.outputs = [("Message", JSONMessage, Single)]
-
-        # Settings...
-        self.autoSend = True
-        self.uuid = None
-        self.loadSettings()
-        self.uuid = getWidgetUuid(self)
+        super().__init__(*args, **kwargs)
 
         # Other attributes...
         self.segmentation = None
         self.infoBox = InfoBox(widget=self.controlArea)
-        OWGUI.separator(self.controlArea, height=3)
+        gui.separator(self.controlArea, height=3)
         self.sendButton = SendButton(
             widget=self.controlArea,
             master=self,
@@ -77,7 +66,7 @@ class OWTextableMessage(OWWidget):
         )
 
         # GUI
-        OWGUI.rubber(self.controlArea)
+        gui.rubber(self.controlArea)
 
         # Send button...
         self.sendButton.draw()
@@ -123,29 +112,14 @@ class OWTextableMessage(OWWidget):
                 u"Please make sure that input contains valid JSON data.",
                 'error'
             )
-            self.send('Segmented data', None, self)
             self.send('Message', None, self)
             return
         self.sendButton.resetSettingsChangedFlag()
 
-    def adjustSizeWithTimer(self):
-        qApp.processEvents()
-        QTimer.singleShot(50, self.adjustSize)
-
-    def getSettings(self, *args, **kwargs):
-        settings = OWWidget.getSettings(self, *args, **kwargs)
-        settings["settingsDataVersion"] = __version__.split('.')[:2]
-        return settings
-
-    def setSettings(self, settings):
-        if settings.get("settingsDataVersion", None) \
-                == __version__.split('.')[:2]:
-            settings = settings.copy()
-            del settings["settingsDataVersion"]
-            OWWidget.setSettings(self, settings)
-
 
 if __name__ == "__main__":
+    import sys
+    from PyQt4.QtGui import QApplication
     appl = QApplication(sys.argv)
     ow = OWTextableMessage()
     ow.show()
