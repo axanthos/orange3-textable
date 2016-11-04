a03d30823d0a1a1f16169d69e6d30c97f73867eb
Aris Xanthos <aris.xanthos@unil.ch>
2016-11-04 16:01:12 +0100

    updated doc author and (c)

--- docs/doc/conf.py
+++ docs/doc/conf.py
@@ -55,8 +55,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Orange3-Textable'
-copyright = '2016, Xanthos, Aris'
-author = 'Xanthos, Aris'
+copyright = '2016, LangTech Sarl'
+author = 'LangTech Sarl'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -266,7 +266,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'Orange3-Textable.tex', 'Orange3-Textable Documentation',
-     'Xanthos, Aris', 'manual'),
+     'LangTech Sarl', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
