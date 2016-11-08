33971ebeac127ee04de853f9fb231ea903c609f3
Aris Xanthos <aris.xanthos@unil.ch>
2016-11-08 15:38:44 +0100

    Updated widget icons and version numbers

--- _textable/widgets/OWTextableRecode.py
+++ _textable/widgets/OWTextableRecode.py
@@ -2,23 +2,23 @@
 Class OWTextableRecode
 Copyright 2012-2016 LangTech Sarl (info@langtech.ch)
 -----------------------------------------------------------------------------
-This file is part of the Orange-Textable package v2.0.
+This file is part of the Orange-Textable package v3.0.
 
-Orange-Textable v2.0 is free software: you can redistribute it and/or modify
+Orange-Textable v3.0 is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Orange-Textable v2.0 is distributed in the hope that it will be useful,
+Orange-Textable v3.0 is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Orange-Textable v2.0. If not, see <http://www.gnu.org/licenses/>.
+along with Orange-Textable v3.0. If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.13.2'  # TODO change subversion?
+__version__ = '0.13.3'
 
 import os, re, codecs, json
 
