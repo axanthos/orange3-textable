46e3fa59825627d93cddc43040c845d3707a73a5
Aris Xanthos <aris.xanthos@unil.ch>
2020-09-17 15:32:48 +0200

    v3.1.8

--- setup.py
+++ setup.py
@@ -29,12 +29,12 @@ if sys.version_info < (3, ):
 
 from setuptools import setup, find_packages
 
-__version__ = "1.0.33"   # file version
+__version__ = "1.0.34"   # file version
 
 NAME = 'Orange3-Textable'
 DOCUMENTATION_NAME = 'Textable'
 
-VERSION = '3.1.7'  # package version
+VERSION = '3.1.8'  # package version
 
 DESCRIPTION = 'Textable add-on for Orange 3 data mining software package.'
 LONG_DESCRIPTION = open(
